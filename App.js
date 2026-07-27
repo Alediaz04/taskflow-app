79afe132cf702c9eb71f9850e0b80592eb41c5b3
Alediaz04 <alejandrodanieldiaz2004@gmail.com>
2026-07-26 22:28:15 -0300

    Nuevo guardado con carpetas creadas localmente

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleShet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App() { 
+    return ( 
+        <View style = { styles.container}>
+            <Text style= {styles.title} > TASKFLOW </Text>
+            <Text style= {styles.subtitle} > Estructura Base </Text>
+            <Text style= {styles.status} > Estructura de la base completa </Text>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: { 
+        flex: 1,
+        backgroundColor: '#0F172A',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: { 
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: '#38BDF8',
+        marginBottom: 8,
+    },
+    subtitle: { 
+        fontSize: 18,
+        color: '#94A3B8',
+        marginBottom: 24,
+
+    },
+    status: {
+        fontSize: 14,
+        color: '#4ADE80',
+        fontWeight: '600',
+    },
+
 });
+
---
+++ README.md
@@ -0,0 +1,27 @@
+# TASKFLOW
+
+Aplicacion movil desarrollada con React Native y Expo para gestionar tareas, habitos y metas personales
+
+## instrucciones para abrir el proyecto localmente
+
+    clonar el repositorio de github git clone cd taskflow-app
+
+    instalar dependencias del proyecto npm install
+
+    iniciar el servidor npx expo start
+
+    Escanear el codigo QR en la app de expo GO
+
+## ESTRUCTURA INICIAL DEL PROYECTO
+
+taskflow-app 
+-- src/ 
+   -- assets/ # recursos como imagenes o fuentes 
+   -- components/ # componentes reutilizables de la UI 
+   -- screens/ # pantallas principales de la aplicacion 
+   -- theme/ # paleta de colores y estilos globales 
+-- .gitignore # archivos y historial ignorados para git
+-- App.js # Entrada de la app 
+-- app.json # configuracion global y metadatos de Expo 
+-- package.json # dependencias del proyecto
+-- README.md #documentacion del proyecto
